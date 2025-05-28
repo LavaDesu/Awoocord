d8bfd28dd745872e14ecc489f4890bd0c07fad38
LavaDesu <me@lava.moe>
2025-05-29 03:44:07 +1000

    chore(Scout): add to updater json

--- plugins/Scout/build.gradle.kts
+++ plugins/Scout/build.gradle.kts
@@ -7,11 +7,5 @@ aliucord {
         1.0.0 - Initial release >w<
     """.trimIndent())
 
-    // Add additional authors to this plugin
-    // author("Name", 0)
-    // author("Name", 0)
-
-    // Excludes this plugin from the updater, meaning it won't show up for users.
-    // Set this if the plugin is unfinished
-    excludeFromUpdaterJson.set(true)
+    excludeFromUpdaterJson.set(false)
 }
