2fdeef02805cd9d0c96199f3b468771cb2626228
LavaDesu <me@lava.moe>
2025-05-29 17:18:13 +1000

    chore(Scout): 1.0.1

--- plugins/Scout/build.gradle.kts
+++ plugins/Scout/build.gradle.kts
@@ -1,10 +1,14 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Backported and improved search functionality"
 
 aliucord {
     // Changelog of your plugin
     changelog.set("""
-        1.0.0 - Initial release >w<
+        # 1.0.1
+        * Fix not being able to search more than one page with sort:old
+
+        # 1.0.0
+        * Initial release >w<
     """.trimIndent())
 
     excludeFromUpdaterJson.set(false)
