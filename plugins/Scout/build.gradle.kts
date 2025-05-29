5c06ee486e3185037db930e72c91250f7dd25844
LavaDesu <me@lava.moe>
2025-05-29 23:40:21 +1000

    chore(Scout): 1.1.0

--- plugins/Scout/build.gradle.kts
+++ plugins/Scout/build.gradle.kts
@@ -1,9 +1,13 @@
-version = "1.0.1"
+version = "1.1.0"
 description = "Backported and improved search functionality"
 
 aliucord {
     // Changelog of your plugin
     changelog.set("""
+        # 1.1.0 - Look out, Scout has:updates
+        * Add "has:forward" and "has:poll" filters
+        * Add "exclude:" filter. It is the opposite of "has:" and filters out matching elements
+
         # 1.0.1
         * Fix not being able to search more than one page with sort:old
 
