386a754b987f77943ce578d21a926e0d25429183
LavaDesu <me@lava.moe>
2025-06-14 01:23:18 +1000

    chore(Scout): 1.1.3

--- plugins/Scout/build.gradle.kts
+++ plugins/Scout/build.gradle.kts
@@ -4,6 +4,9 @@ description = "Backported and improved search functionality"
 aliucord {
     // Changelog of your plugin
     changelog.set("""
+        # 1.1.3
+        * Require restarts on plugin changes
+
         # 1.1.2
         * Fix month being one month behind after using the date picker
 
