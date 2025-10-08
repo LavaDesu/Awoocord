61037e405fca4c6c837ccae70eb0b42e7d7fb84b
Cilly Leang <me@lava.moe>
2025-10-08 17:32:05 +1100

    chore(Zinnia): 1.1.0

--- plugins/Zinnia/build.gradle.kts
+++ plugins/Zinnia/build.gradle.kts
@@ -1,9 +1,13 @@
-version = "1.0.0"
+version = "1.1.0"
 description = "Coloured usernames to be a bit more pleasing on the eyes"
 
 aliucord {
     // Changelog of your plugin
     changelog.set("""
+        # 1.1.0
+        * Fix incorrect spacing in replies
+        * Allow setting static text colours
+
         # 1.0.0
         * Initial release >w<
     """.trimIndent())
