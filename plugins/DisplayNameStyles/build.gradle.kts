7b95c76e63755924f1ef73cae624848157caba0f
Cilly Leang <mini@cilly.moe>
2026-05-02 22:10:12 +1000

    chore(DisplayNameStyles): 0.2.2

--- plugins/DisplayNameStyles/build.gradle.kts
+++ plugins/DisplayNameStyles/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "0.2.1"
+version = "0.2.2"
 description = "Display name styles"
 
 android {
@@ -8,6 +8,10 @@ android {
 aliucord {
     // Changelog of your plugin
     changelog.set("""
+        # 0.2.2
+        * Make font a bit bolder for those on old devices (Android 8 or older) — unfortunately won't be as good as intended but this is the best I could do
+        * Fix font flashing and replies' text overlapping
+
         # 0.2.1
         * Fix app breaking sometimes
         * Actually disable the plugin if it is disabled
